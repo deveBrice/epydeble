180de4b33e766d2cbe568cdc6a72e661d2096ba5
aeki <aekiwow@hotmail.fr>
2019-12-06 10:34:01 +0100

    horodatage et localisation

--- scan.py
+++ scan.py
@@ -1,15 +1,21 @@
-import bluetooth, json
-from blscan import get_nearby_devices, get_rssi_for_devices
+import bluetooth, json, datetime
+from blscan import get_nearby_devices#, get_rssi_for_devices
+import geoip2.database
 
 print("Performing inquiry ... ", end='', flush=True)
 
 nearby_devices = get_nearby_devices()
-nearby_devices = {row[0]: {"name": row[1]} for row in nearby_devices}
+current_datetime = datetime.datetime.now()
+reader = geoip2.database.Reader('./GeoLite2/GeoLite2-City.mmdb')
+response = reader.city('109.14.186.24')
+time = "{}-{}-{} {}:{}:{}".format(current_datetime.year, current_datetime.month, current_datetime.day, current_datetime.hour, current_datetime.minute, current_datetime.second)
+nearby_devices = {row[0]: {"name": row[1], "datetime": time, "country": response.city.name} for row in nearby_devices}
+reader.close()
 
 print("Found {} devices".format(len(nearby_devices)))
 print("Getting RSSI ... ", end='', flush=True)
 
-get_rssi_for_devices(nearby_devices)
+#get_rssi_for_devices(nearby_devices)
 
 print("Done")
 print("Logging ... ", end='', flush=True)
